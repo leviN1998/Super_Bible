048853308026e2b2d7b2e8c99da1c99a6c0357a1
leviN1998 <levin.kolmar@googlemail.com>
2017-07-02 02:39:27 +0200

    Initial commit

---
+++ src/de/levin/lernen/FirstMain.java
@@ -0,0 +1,53 @@
+package de.levin.lernen;
+
+
+import de.levin.lernen.glfw.GLFWManager;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL11;
+
+/**
+ * Created by levin on 01.07.2017.
+ */
+public class FirstMain implements Runnable{
+
+
+    @Override
+    public void run() {
+        GLFWManager.init(1280, 720);
+        init();
+        GLFWManager.initLoop();
+        while (!GLFW.glfwWindowShouldClose(GLFWManager.window)){
+
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+            GLFW.glfwSwapBuffers(GLFWManager.window);
+
+            update();
+            render();
+
+            GLFW.glfwPollEvents();
+        }
+        cleanUp();
+        GLFWManager.cleanUp();
+    }
+
+    public void init(){
+
+    }
+
+    public void update(){
+
+    }
+
+    public void render(){
+
+    }
+
+    public void cleanUp(){
+
+    }
+
+
+    public static void main(String[] args){
+        new FirstMain().run();
+    }
+}
---
+++ src/de/levin/lernen/glfw/GLFWManager.java
@@ -0,0 +1,84 @@
+package de.levin.lernen.glfw;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFW;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.*;
+import static org.lwjgl.system.MemoryUtil.*;
+/**
+ * Created by levin on 01.07.2017.
+ */
+public class GLFWManager {
+
+    public static long window;
+
+    public static void init(int width, int height){
+        System.out.println("LWJGL Version: " + Version.getVersion());
+
+        GLFWErrorCallback.createPrint(System.err).set();
+
+        if(!glfwInit())
+            throw new IllegalStateException("Unable to initialize GLFW");
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+
+        window = glfwCreateWindow(width, height, "Test", NULL, NULL);
+        if (window == NULL) throw new RuntimeException("Failed to create GLFW window");
+
+        glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+                glfwSetWindowShouldClose(window, true);
+        });
+
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1); // int*
+            IntBuffer pHeight = stack.mallocInt(1); // int*
+
+            // Get the window size passed to glfwCreateWindow
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            // Get the resolution of the primary monitor
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            // Center the window
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        } // the stack frame is popped automatically
+
+        // Make the OpenGL context current
+        glfwMakeContextCurrent(window);
+        // Enable v-sync
+        glfwSwapInterval(1);
+
+        // Make the window visible
+        glfwShowWindow(window);
+    }
+
+    public static void initLoop(){
+        GL.createCapabilities();
+        glClearColor(0,1,1,1);
+    }
+
+    public static void cleanUp(){
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+}
